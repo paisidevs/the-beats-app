0d895f0d9b9069fec4b98f43df36b877295dd927
Thandolwethu Mpofu <mpofutthandolwethu@gmail.om>
2020-04-27 00:48:56 +0200

    feat(frontend): setup frontend

---
+++ packages/frontend/src/apollo.config.ts
@@ -0,0 +1,68 @@
+import {
+  ApolloClient,
+  errorLink,
+  from,
+  gql,
+  HttpLink,
+  InMemoryCache,
+  loggerLink,
+  RetryLink,
+  setContext,
+} from "@paisidevs/tra-apollo";
+import { persistCache } from "apollo-cache-persist";
+import { PersistentStorage } from "apollo-cache-persist/types";
+import { GRAPHQL_ENDPOINT, NODE_ENV } from "./constants";
+import { getCognitoUserToken } from "./utilities";
+
+const cache = new InMemoryCache();
+
+const httpLink = new HttpLink({
+  uri: GRAPHQL_ENDPOINT,
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from (local/session) storage if it exists
+  const token = getCognitoUserToken();
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const retryLink = new RetryLink();
+
+let clientLink = from([retryLink, authLink, httpLink]);
+
+if (NODE_ENV === "development") {
+  // @ts-ignore - tra-apollo types compatibility issues
+  clientLink = from([loggerLink, errorLink, clientLink]);
+}
+
+export const client = new ApolloClient({
+  cache,
+  // @ts-ignore - tra-apollo types compatibility issues
+  link: clientLink,
+});
+
+export const persistedClient = async () => {
+  await persistCache({
+    cache,
+    storage: window.localStorage as PersistentStorage<string | null>,
+  });
+
+  return client;
+};
+
+cache.writeQuery({
+  query: gql`
+    query {
+      todos
+    }
+  `,
+  data: {
+    todos: [],
+  },
+});
---
+++ packages/frontend/src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Logo } from "../../components";
+import { Wrapper } from "./styles";
+
+const Home: React.FC = () => {
+  return (
+    <Wrapper>
+      <Helmet>
+        <title>the-react-app - Built with love by @paisidevs</title>
+      </Helmet>
+      <Logo />
+    </Wrapper>
+  );
+};
+
+export default Home;
