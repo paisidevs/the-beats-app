a5e3a4951c720f744cb1a55423ff3fb751b10308
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2020-07-13 14:48:52 +0200

    Bunch of changes

--- packages/frontend/src/pages/Songs/index.tsx
+++ packages/frontend/src/pages/Songs/index.tsx
@@ -1,4 +1,5 @@
-import { Animated, Box, ScrollView } from "@paisidevs/tra-components";
+import { Animated, Box, Loader, ScrollView } from "@paisidevs/tra-components";
+import { logger } from "@paisidevs/tra-utilities";
 import React, { FC } from "react";
 import { Track } from "../../components";
 import { useTracksQuery } from "../../generated/graphql";
@@ -6,15 +7,19 @@ import { useAnimations } from "../../hooks";
 
 const Songs: FC = () => {
   const { useFadeUp } = useAnimations();
-  const { data } = useTracksQuery({
-    fetchPolicy: "cache-and-network",
+  const { data, loading } = useTracksQuery({
     variables: { sort: "name:asc" },
   });
+  logger({ data });
+
+  if (loading || !data) {
+    return <Loader />;
+  }
 
   return (
     <ScrollView>
       <Animated style={useFadeUp} paddingX={2} width="100%">
-        {data?.tracks?.map((track, index) =>
+        {data.tracks?.map((track, index) =>
           track ? (
             <Box
               key={track.id}
